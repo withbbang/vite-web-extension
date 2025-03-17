e44752eba7485edb0453831d92ca7baceff03b0b
김영선 <withbbang@gmail.com>
2025-03-18 00:08:52 +0900

    Chore: content_scripts.ts build를 포함시키기 위한 코드 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import webExtension, { readJsonFile } from "vite-plugin-web-extension";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import webExtension, { readJsonFile } from 'vite-plugin-web-extension';
+import path from 'path';
 
 function generateManifest() {
-  const manifest = readJsonFile("src/manifest.json");
-  const pkg = readJsonFile("package.json");
+  const manifest = readJsonFile('src/manifest.json');
+  const pkg = readJsonFile('package.json');
   return {
     name: pkg.name,
     description: pkg.description,
@@ -13,12 +14,17 @@ function generateManifest() {
   };
 }
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     webExtension({
       manifest: generateManifest,
+      additionalInputs: ['src/utils/content_scripts.ts'],
     }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
